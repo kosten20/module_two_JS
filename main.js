728822af0ae07769bb75ca7c11d26dede704385b
kosten <96771947+kosten20@users.noreply.github.com>
2023-02-03 08:22:48 +0600

    implemented task

---
+++ main.js
@@ -0,0 +1,11 @@
+let money = 1200;
+let profit = "null";
+let expenses = "life";
+let purpose = 45000;
+let period = 2;
+
+console.log(`тип данных money "${typeof money}", тип данных profit "${typeof profit}"`);
+console.log(`период равен ${period} месяцев, Цель заработать ${purpose} рублей`);
+let budGetDay = 30;
+console.log(budGetDay);
+
